013564370e6d316bb1258dadc6822a8e8cb2ea2d
Eduardo Perin <89282463+perinedu@users.noreply.github.com>
2026-05-30 00:25:38 -0300

    Add files via upload

---
+++ missaoS1.js
@@ -0,0 +1,351 @@
+window.MISSAO = {
+  "gerado_em": "2026-05-29T23:10:58",
+  "servidor": "1",
+  "missoes": [
+    {
+      "titulo": "Fugura Surpresa 1",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "PONTOS NO TORNEIO DE GVG",
+          "sigla": "",
+          "qtd": "20000"
+        },
+        {
+          "nome": "FORMIGUEIRO FÁCIL",
+          "sigla": "FF",
+          "qtd": "10"
+        },
+        {
+          "nome": "IR BATALHA DE CASAL",
+          "sigla": "",
+          "qtd": "35"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 2",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "PONTOS CF (QUALQUER)",
+          "sigla": "",
+          "qtd": "35000"
+        },
+        {
+          "nome": "GANHAR GVG EM QUALQUER MODO",
+          "sigla": "",
+          "qtd": "35"
+        },
+        {
+          "nome": "BUGOU NORMAL",
+          "sigla": "BN",
+          "qtd": "10"
+        },
+        {
+          "nome": "CONSUMO DE CUPONS",
+          "sigla": "",
+          "qtd": "3500"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 3",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "IR 4X4 LIVRE",
+          "sigla": "",
+          "qtd": "7"
+        },
+        {
+          "nome": "BUGOU AVANÇADO",
+          "sigla": "BA",
+          "qtd": "10"
+        },
+        {
+          "nome": "GALINHEIRO FÁCIL",
+          "sigla": "PF",
+          "qtd": "10"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 4",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "DERROTAR NO DANO NO TORNEIO GVG",
+          "sigla": "",
+          "qtd": "35"
+        },
+        {
+          "nome": "FORMIGUEIRO NORMAL",
+          "sigla": "FN",
+          "qtd": "10"
+        },
+        {
+          "nome": "BUGOU DIFÍCIL",
+          "sigla": "BD",
+          "qtd": "5"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 5",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "GANHAR PVP EQUILIBRADO",
+          "sigla": "",
+          "qtd": "35"
+        },
+        {
+          "nome": "CASTELO SOMBRIO AVANÇADO",
+          "sigla": "CSA",
+          "qtd": "10"
+        },
+        {
+          "nome": "COVIL DRAGÃO AVANÇADO",
+          "sigla": "CDA",
+          "qtd": "10"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 6",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "GALINHEIRO DIFÍCIL",
+          "sigla": "PD",
+          "qtd": "5"
+        },
+        {
+          "nome": "DERROTAR NO DANO GVG EQUILIBRADO",
+          "sigla": "",
+          "qtd": "50"
+        },
+        {
+          "nome": "COVIL DRAGÃO DIFÍCIL",
+          "sigla": "CDD",
+          "qtd": "10"
+        },
+        {
+          "nome": "TRIBO PIATAN DIFÍCIL",
+          "sigla": "TPD",
+          "qtd": "10"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 7",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "COMPOSIÇÃO DEFESA 150",
+          "sigla": "",
+          "qtd": "3"
+        },
+        {
+          "nome": "CASTELO SOMBRIO DIFÍCIL",
+          "sigla": "CSD",
+          "qtd": "10"
+        },
+        {
+          "nome": "FICAR NO SPA",
+          "sigla": "",
+          "qtd": "240"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 8",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "RECARREGAR PRIMEIRA VEZ",
+          "sigla": "",
+          "qtd": "1100"
+        },
+        {
+          "nome": "GANHAR GVG EQUILIBRADO",
+          "sigla": "",
+          "qtd": "35"
+        },
+        {
+          "nome": "CASTELO SOMBRIO NORMAL",
+          "sigla": "CSN",
+          "qtd": "10"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 9",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "OLIMPÍADAS BOOM AVANÇADO",
+          "sigla": "OBA",
+          "qtd": "10"
+        },
+        {
+          "nome": "VENCER NA ARENA 1 VEZES",
+          "sigla": "",
+          "qtd": "35"
+        },
+        {
+          "nome": "COMPOSIÇÃO AGILIDADE 150",
+          "sigla": "",
+          "qtd": "5"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 10",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "IR PVP 3X3 LIVRE",
+          "sigla": "",
+          "qtd": "7"
+        },
+        {
+          "nome": "TEMPO ONLINE",
+          "sigla": "",
+          "qtd": "600"
+        },
+        {
+          "nome": "GALINHEIRO NORMAL",
+          "sigla": "PN",
+          "qtd": "15"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 11",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "PONTOS SUGAR BUGOU",
+          "sigla": "",
+          "qtd": "250000"
+        },
+        {
+          "nome": "TRIBO PIATAN AVANÇADO",
+          "sigla": "TPA",
+          "qtd": "15"
+        },
+        {
+          "nome": "ACUMULAR RIQUEZA NA SOCIEDADE",
+          "sigla": "",
+          "qtd": "3500"
+        }
+      ]
+    },
+    {
+      "titulo": "Fugura Surpresa 12",
+      "inicio": "10-08-2025",
+      "fim": "30-09-2026",
+      "tarefas": [
+        {
+          "nome": "DERROTAR NO DANO EM GVG LIVRE",
+          "sigla": "",
+          "qtd": "35"
+        },
+        {
+          "nome": "PONTOS CF FUTEBOL",
+          "sigla": "",
+          "qtd": "35000"
+        },
+        {
+          "nome": "TERMINAR 1 BATALHA NA ARENA",
+          "sigla": "",
+          "qtd": "5"
+        }
+      ]
+    },
+    {
+      "titulo": "O Chamado dos Piratas",
+      "inicio": "19-05-2026",
+      "fim": "03-06-2026",
+      "tarefas": [
+        {
+          "nome": "COVIL DRAGÃO AVANÇADO",
+          "sigla": "CDA",
+          "qtd": "5"
+        },
+        {
+          "nome": "PONTOS CF FUTEBOL",
+          "sigla": "",
+          "qtd": "1000"
+        },
+        {
+          "nome": "TRIBO PIATAN AVANÇADO",
+          "sigla": "TPA",
+          "qtd": "5"
+        }
+      ]
+    },
+    {
+      "titulo": "Caçada ao Tesouro Perdido",
+      "inicio": "19-05-2026",
+      "fim": "03-06-2026",
+      "tarefas": [
+        {
+          "nome": "GALINHEIRO FÁCIL",
+          "sigla": "PF",
+          "qtd": "2"
+        },
+        {
+          "nome": "IR GVG EM MODO NORMAL",
+          "sigla": "",
+          "qtd": "10"
+        },
+        {
+          "nome": "FICAR NO SPA",
+          "sigla": "",
+          "qtd": "10"
+        }
+      ]
+    },
+    {
+      "titulo": "Copinha ",
+      "inicio": "23-05-2026",
+      "fim": "07-06-2026",
+      "tarefas": [
+        {
+          "nome": "PONTOS CF FUTEBOL",
+          "sigla": "",
+          "qtd": "10000"
+        },
+        {
+          "nome": "IR PVP 3X3 LIVRE",
+          "sigla": "",
+          "qtd": "7"
+        },
+        {
+          "nome": "VENCER NA ARENA 1 VEZES",
+          "sigla": "",
+          "qtd": "10"
+        },
+        {
+          "nome": "TERMINAR 1 BATALHA NA ARENA",
+          "sigla": "",
+          "qtd": "2"
+        }
+      ]
+    }
+  ]
+};
---
+++ missaoS13.js
@@ -0,0 +1,168 @@
+window.MISSAO = {
+  "gerado_em": "2026-05-30T00:18:06",
+  "servidor": "13",
+  "missoes": [
+    {
+      "titulo": "Quinta Feira",
+      "inicio": "14-05-2026",
+      "fim": "30-05-2026",
+      "tarefas": [
+        {
+          "nome": "IR TORNEIO 2X2",
+          "sigla": "",
+          "qtd": "10"
+        },
+        {
+          "nome": "BUGOU DIFÍCIL",
+          "sigla": "BD",
+          "qtd": "5"
+        },
+        {
+          "nome": "COVIL DRAGÃO NORMAL",
+          "sigla": "CDN",
+          "qtd": "3"
+        },
+        {
+          "nome": "PONTOS CF (QUALQUER)",
+          "sigla": "",
+          "qtd": "2000"
+        }
+      ]
+    },
+    {
+      "titulo": "Azul",
+      "inicio": "23-05-2026",
+      "fim": "07-06-2026",
+      "tarefas": [
+        {
+          "nome": "GANHAR COMBATE COM 1 DE VIDA",
+          "sigla": "",
+          "qtd": "5"
+        },
+        {
+          "nome": "COVIL DRAGÃO DIFÍCIL",
+          "sigla": "CDD",
+          "qtd": "3"
+        },
+        {
+          "nome": "FORMIGUEIRO NORMAL",
+          "sigla": "FN",
+          "qtd": "5"
+        },
+        {
+          "nome": "DERROTAR NO DANO NO TORNEIO GVG",
+          "sigla": "",
+          "qtd": "25"
+        }
+      ]
+    },
+    {
+      "titulo": "Verde",
+      "inicio": "23-05-2026",
+      "fim": "07-06-2026",
+      "tarefas": [
+        {
+          "nome": "IR BATALHA DE CASAL",
+          "sigla": "",
+          "qtd": "15"
+        },
+        {
+          "nome": "DERROTAR NO DANO EM PVP LIVRE",
+          "sigla": "",
+          "qtd": "15"
+        },
+        {
+          "nome": "CASTELO SOMBRIO AVANÇADO",
+          "sigla": "CSA",
+          "qtd": "2"
+        },
+        {
+          "nome": "TRIBO PIATAN NORMAL",
+          "sigla": "TPN",
+          "qtd": "3"
+        }
+      ]
+    },
+    {
+      "titulo": "Terça",
+      "inicio": "26-05-2026",
+      "fim": "13-06-2026",
+      "tarefas": [
+        {
+          "nome": "IR BATALHA DE CASAL",
+          "sigla": "",
+          "qtd": "15"
+        },
+        {
+          "nome": "CASTELO SOMBRIO NORMAL",
+          "sigla": "CSN",
+          "qtd": "2"
+        },
+        {
+          "nome": "TRIBO PIATAN DIFÍCIL",
+          "sigla": "TPD",
+          "qtd": "3"
+        },
+        {
+          "nome": "PONTOS SUGAR BUGOU",
+          "sigla": "",
+          "qtd": "50000"
+        }
+      ]
+    },
+    {
+      "titulo": "Quinta",
+      "inicio": "28-05-2026",
+      "fim": "12-06-2026",
+      "tarefas": [
+        {
+          "nome": "FICAR NO SPA",
+          "sigla": "",
+          "qtd": "60"
+        },
+        {
+          "nome": "GANHAR COMBATE COM 1 DE VIDA",
+          "sigla": "",
+          "qtd": "10"
+        },
+        {
+          "nome": "OLIMPÍADAS BOOM AVANÇADO",
+          "sigla": "OBA",
+          "qtd": "1"
+        },
+        {
+          "nome": "CASTELO SOMBRIO DIFÍCIL",
+          "sigla": "CSD",
+          "qtd": "3"
+        }
+      ]
+    },
+    {
+      "titulo": "Final de Semana",
+      "inicio": "30-05-2026",
+      "fim": "31-05-2026",
+      "tarefas": [
+        {
+          "nome": "NÃO DESCOBERTA",
+          "sigla": "",
+          "qtd": "??"
+        },
+        {
+          "nome": "GALINHEIRO DIFÍCIL",
+          "sigla": "PD",
+          "qtd": "3"
+        },
+        {
+          "nome": "COVIL DRAGÃO DIFÍCIL",
+          "sigla": "CDD",
+          "qtd": "3"
+        },
+        {
+          "nome": "NÃO DESCOBERTA",
+          "sigla": "",
+          "qtd": "??"
+        }
+      ]
+    }
+  ]
+};
