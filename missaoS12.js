b61bb6a7d413c8843bb0d74960cf1a8c5a11c926
Eduardo Perin <89282463+perinedu@users.noreply.github.com>
2026-06-04 15:42:54 -0300

    Update missaoS12.js

--- missaoS12.js
+++ missaoS12.js
@@ -1,111 +1,29 @@
 window.MISSAO = {
-  "gerado_em": "2026-05-29T23:07:05",
   "servidor": "12",
   "missoes": [
     {
-      "titulo": "Semanal A",
-      "inicio": "05-05-2026",
-      "fim": "30-05-2026",
+      "titulo": "Copinha Surpresa",
+      "inicio": "04-06-2026",
+      "fim": "20-06-2026",
       "tarefas": [
         {
-          "nome": "CASTELO SOMBRIO AVANÇADO",
-          "sigla": "CSA",
+          "nome": "NÃO DESCOBERTA",
+          "sigla": "",
+          "qtd": "??"
+        },
+        {
+          "nome": "BUGOU FÁCIL",
+          "sigla": "BF",
           "qtd": "5"
         },
         {
-          "nome": "TRIBO PIATAN AVANÇADO",
-          "sigla": "TPA",
-          "qtd": "5"
+          "nome": "GALINHEIRO FÁCIL",
+          "sigla": "PF",
+          "qtd": "6"
         },
         {
-          "nome": "PONTOS CF (QUALQUER)",
-          "sigla": "",
-          "qtd": "5000"
-        },
-        {
-          "nome": "FICAR NO SPA",
-          "sigla": "",
-          "qtd": "120"
-        }
-      ]
-    },
-    {
-      "titulo": "Semanal B",
-      "inicio": "05-05-2026",
-      "fim": "30-05-2026",
-      "tarefas": [
-        {
-          "nome": "VENCER NA ARENA 1 VEZES",
-          "sigla": "",
-          "qtd": "25"
-        },
-        {
-          "nome": "BUGOU AVANÇADO",
-          "sigla": "BA",
-          "qtd": "5"
-        },
-        {
-          "nome": "COVIL DRAGÃO DIFÍCIL",
-          "sigla": "CDD",
-          "qtd": "5"
-        },
-        {
-          "nome": "IR BATALHA DE CASAL",
-          "sigla": "",
-          "qtd": "25"
-        }
-      ]
-    },
-    {
-      "titulo": "Semanal C",
-      "inicio": "05-05-2026",
-      "fim": "30-05-2026",
-      "tarefas": [
-        {
-          "nome": "FORMIGUEIRO FÁCIL",
-          "sigla": "FF",
-          "qtd": "15"
-        },
-        {
-          "nome": "DERROTAR NO DANO GVG EQUILIBRADO",
-          "sigla": "",
-          "qtd": "35"
-        },
-        {
-          "nome": "OLIMPÍADAS BOOM NORMAL",
-          "sigla": "OBN",
-          "qtd": "5"
-        },
-        {
-          "nome": "PONTOS SUGAR BUGOU",
-          "sigla": "",
-          "qtd": "150000"
-        }
-      ]
-    },
-    {
-      "titulo": "Semanal D",
-      "inicio": "05-05-2026",
-      "fim": "30-05-2026",
-      "tarefas": [
-        {
-          "nome": "GANHAR TORNEIO GVG",
-          "sigla": "",
-          "qtd": "25"
-        },
-        {
-          "nome": "GANHAR COMBATE COM 1 DE VIDA",
-          "sigla": "",
-          "qtd": "10"
-        },
-        {
-          "nome": "GALINHEIRO NORMAL",
-          "sigla": "PN",
-          "qtd": "5"
-        },
-        {
-          "nome": "TRIBO PIATAN AVANÇADO",
-          "sigla": "TPA",
+          "nome": "TRIBO PIATAN NORMAL",
+          "sigla": "TPN",
           "qtd": "5"
         }
       ]
