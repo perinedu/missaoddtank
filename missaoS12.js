0def4a3b871f1327584a8dc4041b8543f87870bb
Eduardo Perin <89282463+perinedu@users.noreply.github.com>
2026-06-09 10:56:41 -0300

    Update missaoS12.js

--- missaoS12.js
+++ missaoS12.js
@@ -1,32 +1,356 @@
 window.MISSAO = {
-  "servidor": "12",
   "missoes": [
     {
-      "titulo": "Copinha Surpresa",
-      "inicio": "04-06-2026",
-      "fim": "20-06-2026",
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
       "tarefas": [
         {
-          "nome": "PONTOS CF DRAGÃO",
-          "sigla": "",
-          "qtd": "1000"
+          "nome": "",
+          "qtd": "7",
+          "sigla": ""
         },
         {
-          "nome": "BUGOU FÁCIL",
-          "sigla": "BF",
-          "qtd": "5"
+          "nome": "COVIL DRAGÃO NORMAL",
+          "qtd": "5",
+          "sigla": "CDN"
+        },
+        {
+          "nome": "IR TORNEIO 2X2",
+          "qtd": "30",
+          "sigla": ""
+        },
+        {
+          "nome": "",
+          "qtd": "5",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 1"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "GANHAR PVP EQUILIBRADO",
+          "qtd": "30",
+          "sigla": ""
+        },
+        {
+          "nome": "OLIMPÍADAS BOOM NORMAL",
+          "qtd": "5",
+          "sigla": "OBN"
+        },
+        {
+          "nome": "IR PVP EQUILIBRADO",
+          "qtd": "30",
+          "sigla": ""
+        },
+        {
+          "nome": "",
+          "qtd": "5",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 2"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "PONTOS CF FUTEBOL",
+          "qtd": "12500",
+          "sigla": ""
+        },
+        {
+          "nome": "GANHAR GVG 2X2 (MODO LIVRE)",
+          "qtd": "30",
+          "sigla": ""
+        },
+        {
+          "nome": "TRIBO PIATAN DIFÍCIL",
+          "qtd": "5",
+          "sigla": "TPD"
+        },
+        {
+          "nome": "TERMINAR 1 BATALHA NA ARENA",
+          "qtd": "7",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 3"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "FAZER LOGIN NO PASSE BRASIL AMANHÃ",
+          "qtd": "1",
+          "sigla": ""
+        },
+        {
+          "nome": "",
+          "qtd": "7",
+          "sigla": ""
+        },
+        {
+          "nome": "FICAR NO SPA",
+          "qtd": "360",
+          "sigla": ""
+        },
+        {
+          "nome": "GANHAR COMBATE COM 1 DE VIDA",
+          "qtd": "10",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 4"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "CASTELO SOMBRIO AVANÇADO",
+          "qtd": "5",
+          "sigla": "CSA"
+        },
+        {
+          "nome": "IR 4X4 LIVRE",
+          "qtd": "7",
+          "sigla": ""
+        },
+        {
+          "nome": "CORSÁRIO",
+          "qtd": "1",
+          "sigla": "CORSÁRIO"
+        },
+        {
+          "nome": "GANHAR TORNEIO GVG",
+          "qtd": "30",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 5"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "DERROTAR NO DANO EM PVP LIVRE",
+          "qtd": "35",
+          "sigla": ""
+        },
+        {
+          "nome": "PONTOS CF DRAGÃO",
+          "qtd": "15900",
+          "sigla": ""
+        },
+        {
+          "nome": "IR GVG EM MODO EQUILIBRADO",
+          "qtd": "25",
+          "sigla": ""
+        },
+        {
+          "nome": "",
+          "qtd": "5",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 6"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "TRIBO PIATAN NORMAL",
+          "qtd": "5",
+          "sigla": "TPN"
+        },
+        {
+          "nome": "BUGOU DIFÍCIL",
+          "qtd": "5",
+          "sigla": "BD"
         },
         {
           "nome": "GALINHEIRO FÁCIL",
-          "sigla": "PF",
-          "qtd": "6"
+          "qtd": "5",
+          "sigla": "PF"
+        },
+        {
+          "nome": "PONTOS SUGAR BUGOU",
+          "qtd": "150000",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 7"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "DERROTAR NO DANO NO TORNEIO GVG",
+          "qtd": "35",
+          "sigla": ""
+        },
+        {
+          "nome": "IR BATALHA DE CASAL",
+          "qtd": "25",
+          "sigla": ""
+        },
+        {
+          "nome": "GALINHEIRO DIFÍCIL",
+          "qtd": "5",
+          "sigla": "PD"
+        },
+        {
+          "nome": "CASTELO SOMBRIO DIFÍCIL",
+          "qtd": "5",
+          "sigla": "CSD"
+        }
+      ],
+      "titulo": "Fugura Surpresa 8"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "",
+          "qtd": "5",
+          "sigla": ""
+        },
+        {
+          "nome": "OLIMPÍADAS BOOM AVANÇADO",
+          "qtd": "5",
+          "sigla": "OBA"
+        },
+        {
+          "nome": "GANHAR PVP 1X1 (MODO LIVRE)",
+          "qtd": "35",
+          "sigla": ""
+        },
+        {
+          "nome": "PONTOS CF VAMPIRO",
+          "qtd": "9500",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 9"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "FORMIGUEIRO FÁCIL",
+          "qtd": "10",
+          "sigla": "FF"
+        },
+        {
+          "nome": "",
+          "qtd": "7",
+          "sigla": ""
+        },
+        {
+          "nome": "RECARREGAR PRIMEIRA VEZ",
+          "qtd": "3500",
+          "sigla": ""
+        },
+        {
+          "nome": "COVIL DRAGÃO NORMAL",
+          "qtd": "5",
+          "sigla": "CDN"
+        }
+      ],
+      "titulo": "Fugura Surpresa 10"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "IR GVG EM MODO NORMAL",
+          "qtd": "35",
+          "sigla": ""
+        },
+        {
+          "nome": "CASTELO SOMBRIO NORMAL",
+          "qtd": "5",
+          "sigla": "CSN"
+        },
+        {
+          "nome": "ACUMULAR RIQUEZA NA SOCIEDADE",
+          "qtd": "5000",
+          "sigla": ""
+        },
+        {
+          "nome": "",
+          "qtd": "1",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 11"
+    },
+    {
+      "fim": "15-10-2026",
+      "inicio": "14-08-2025",
+      "tarefas": [
+        {
+          "nome": "",
+          "qtd": "3",
+          "sigla": ""
+        },
+        {
+          "nome": "CASE-SE COM ELE (A)",
+          "qtd": "1",
+          "sigla": ""
+        },
+        {
+          "nome": "FORMIGUEIRO NORMAL",
+          "qtd": "5",
+          "sigla": "FN"
+        },
+        {
+          "nome": "CONSUMO DE CUPONS",
+          "qtd": "5500",
+          "sigla": ""
+        }
+      ],
+      "titulo": "Fugura Surpresa 12"
+    },
+    {
+      "fim": "20-06-2026",
+      "inicio": "04-06-2026",
+      "tarefas": [
+        {
+          "nome": "PONTOS CF DRAGÃO",
+          "qtd": "1000",
+          "sigla": ""
+        },
+        {
+          "nome": "BUGOU FÁCIL",
+          "qtd": "5",
+          "sigla": "BF"
+        },
+        {
+          "nome": "GALINHEIRO FÁCIL",
+          "qtd": "6",
+          "sigla": "PF"
         },
         {
           "nome": "TRIBO PIATAN NORMAL",
-          "sigla": "TPN",
-          "qtd": "5"
+          "qtd": "5",
+          "sigla": "TPN"
         }
-      ]
+      ],
+      "titulo": "Copinha Surpresa"
     }
-  ]
+  ],
+  "servidor": "12"
 };
